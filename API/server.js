1454e03f42f005762011befbcb33d1fe93cd580f
Seth Acheampong <baffuorakoto137@gmail.com>
2021-12-03 20:43:14 +0000

    initial setup

--- API/server.js
+++ API/server.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+const errorHandler = require("./middleware/error");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
 const app = express();
+const router = express.Router();
+
+//Body parser
+app.use(express.json());
+//Route files
+const auth = require("./routes/auth");
+
+//Mount routers
+app.use(errorHandler);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
 
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
